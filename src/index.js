4199801554dcdfb7cba88b1e05d5e58731365cd7
Yann Durand <yanndurand.contact@gmail.com>
2021-04-18 12:23:34 +0200

    Removing useless file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './index.css';
 
 function AddPersonForm(props) {
   const [ person, setPerson ] = useState('');
