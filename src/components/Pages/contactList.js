623e86dcb0b83094e6d4482299502b6b369670a6
Yann Durand <yanndurand.contact@gmail.com>
2021-04-20 20:54:00 +0200

    Update: Updating contact list render

--- src/components/Pages/contactList.js
+++ src/components/Pages/contactList.js
@@ -6,6 +6,8 @@ function ContactList(props) {
   const listItems = arr.map((val, index) =>
     <div key={ index }>
       { val.firstName + "\n" + val.lastName + " - " + val.phoneNumber }
+      <button onClick='updateContact()'>Update</button>
+      <button onClick='deleteContact()'>Delete</button>
     </div>
   );
   return <ul>{ listItems }</ul>;
